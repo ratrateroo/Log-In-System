549003ecb544d1fcf58a001e96dbdd04dc7e1bae
Mark T <ratrateroo@gmail.com>
2019-07-01 18:52:11 +0800

    added logout features

--- header.php
+++ header.php
@@ -18,12 +18,24 @@ session_start();
                     <li><a href="index.php">Home</a></li>
                 </ul>
                 <div class="nav-login">
-                    <form action="includes/login.inc.php" method="POST">
-                        <input type="text" name="uid" placeholder="Username/e-mail">
-                        <input type="password" name="pwd" placeholder="Password">
-                        <button type="submit" name="submit">Login</button>
-                    </form>
-                    <a href="signup.php">Sign up</a>
+                    <?php 
+                        if (isset($_SESSION['u_id'])) {
+                            echo '<form action="includes/logout.inc.php" method="POST">
+                            <button type="submit" name="submit">Logout</button>
+                        </form>';
+                        } else {
+                            echo ' <form action="includes/login.inc.php" method="POST">
+                            <input type="text" name="uid" placeholder="Username/e-mail">
+                            <input type="password" name="pwd" placeholder="Password">
+                            <button type="submit" name="submit">Login</button>
+                        </form>
+                        <a href="signup.php">Sign up</a>';
+                        }
+                    ?>
+                    
+
+
+                   
                 </div>
             </div>
         </nav>
