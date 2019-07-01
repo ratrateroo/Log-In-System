1d80e4d6da96da8ff6bd60113decbe9e55f109a9
Mark T <ratrateroo@gmail.com>
2019-07-01 15:47:01 +0800

    setup log in page

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="main-wrapper">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                </ul>
+                <div class="nav-login">
+                    <form action="">
+                        <input type="text" name="uid" placeholder="Username/e-mail">
+                        <input type="password" name="pwd" placeholder="Password">
+                        <button type="submit" name="submit">Login</button>
+                    </form>
+                    <a href="signup.php">Sign up</a>
+                </div>
+            </div>
+        </nav>
+    </header>
