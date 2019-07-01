748fc2dec70b804d3f5d29c4a696acf27a651557
Mark T <ratrateroo@gmail.com>
2019-07-01 16:52:42 +0800

    connected to the database

---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServerName = "localhost";
+$dbUsername = "root";
+$dbPassword = "1234";
+$dbName = "login_system_1";
+
+$conn = mysqli_connect($dbServerName, $dbUsername, $dbPassword, $dbName);
+
